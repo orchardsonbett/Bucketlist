ebe368ca0088fe71307fbb0a9541413999097adf
orchardsonbett <orchardsonbett@gmail.com>
2016-08-19 10:15:00 +0300

    added code

--- app.py
+++ app.py
@@ -20,3 +20,22 @@ def main():
 @app.route('/showSignUp')
 def showSignUp():
     return render_template('signup.html')
+
+
+@app.route('/signUp',methods=['POST','GET'])
+def signUp():
+    try:
+        _name = request.form['inputName']
+        _email = request.form['inputEmail']
+        _password = request.form['inputPassword']
+
+        # validate the received values
+        if _name and _email and _password:
+
+            # All Good, let's call MySQL
+
+            conn = mysql.connect()
+            cursor = conn.cursor()
+            _hashed_password = generate_password_hash(_password)
+            cursor.callproc('sp_createUser',(_name,_email,_hashed_password))
+            data = cursor.fetchall()
